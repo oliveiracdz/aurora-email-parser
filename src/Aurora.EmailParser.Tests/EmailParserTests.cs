0282fb814e40f0f7c8696134b90cba14c29d324c
Daniel José Garcia de Oliveira <oliveiracdz@hotmail.com>
2020-07-11 13:24:53 -0300

    Initial commit.

---
+++ src/Aurora.EmailParser.Tests/EmailParserTests.cs
@@ -0,0 +1,61 @@
+using HtmlAgilityPack;
+using Shouldly;
+using System.IO;
+using System.Linq;
+using Xunit;
+
+namespace Aurora.EmailParser.Tests
+{
+    public class EmailParserTests
+    {
+        [Theory]
+        [InlineData("Email-Simples", 3)]
+        [InlineData("Email-Complexo", 2)]
+        public void Quando_HouverQuote_Deve_ExtrairPartes(string file, int chainParts)
+        {
+            var context = Setup(file);
+
+            context.Original.ShouldNotBe(context.Result);
+            context.Chain.Count().ShouldBe(chainParts);
+
+            for (int i = context.Chain.Count() - 1; i > 0;)
+            {
+                var part = context.Chain.ElementAt(i);
+                var prev = context.Chain.ElementAt(--i);
+
+                part.ShouldNotContain(prev);
+            }
+        }
+
+        [Fact]
+        public void Quando_NaoHouverQuote_Deve_RetornarOProprioHtml()
+        {
+            var context = Setup("EmailWithNoQuotes");
+
+            context.Original.ShouldBe(context.Result);
+            context.Chain.Count().ShouldBe(1);
+        }
+
+        private static SetupContext Setup(string teste)
+        {
+            var path = $@"{Directory.GetCurrentDirectory()}\Resources\{teste}.html";
+            var resultHtml = EmailParser.Parse(path);
+            var original = new HtmlDocument(); original.Load(path);
+            var result = new HtmlDocument(); result.LoadHtml(resultHtml.QuoteChain[0]);
+
+            return new SetupContext
+            {
+                Original = original.DocumentNode.OuterHtml,
+                Result = result.DocumentNode.OuterHtml,
+                Chain = resultHtml.QuoteChain
+            };
+        }
+
+        class SetupContext
+        {
+            public string Original { get; set; }
+            public string Result { get; set; }
+            public string[] Chain { get; set; }
+        }
+    }
+}
---
+++ src/Aurora.EmailParser/EmailParser.cs
@@ -0,0 +1,89 @@
+﻿using HtmlAgilityPack;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Aurora.EmailParser
+{
+    public class EmailParser
+    {
+        /* Refs: 
+         * https://www.mailgun.com/blog/open-sourcing-our-email-signature-parsing-library/
+         * https://sigparser.com/
+         */
+
+        public static EmailParsed Parse(string path)
+        {
+            var document = new HtmlDocument();
+            document.Load(path);
+
+            var root = document.DocumentNode.SelectSingleNode("//body") ?? document.DocumentNode;
+            var chain = new[] { ConcatNodes(root.ChildNodes) }.AsEnumerable();
+
+            if (TryFindQuoteNode(root.ChildNodes, out var quote))
+            {
+                chain = ExtractChain(quote.ParentNode).ToList();
+            }
+
+            return new EmailParsed(chain);
+        }
+
+        private static IEnumerable<string> ExtractChain(HtmlNode node)
+        {
+            var previousNodes = new List<HtmlNode>();
+
+            foreach (var item in node.ChildNodes)
+            {
+                var part = item;
+
+                if (IsQuoteNode(item) || node.LastChild == item)
+                {
+                    yield return ConcatNodes(previousNodes);
+
+                    previousNodes.Clear();
+                }
+
+                previousNodes.Add(part);
+            }
+        }
+
+        private static bool TryFindQuoteNode(IEnumerable<HtmlNode> nodes, out HtmlNode quote)
+        {
+            foreach (var node in nodes)
+            {
+                if (IsQuoteNode(node))
+                {
+                    quote = node;
+                    return true;
+                }
+
+                if (TryFindQuoteNode(node.ChildNodes, out quote))
+                {
+                    return true;
+                }
+            }
+
+            quote = null;
+            return false;
+        }
+
+        private static bool IsQuoteNode(HtmlNode node)
+        {
+            var text = SanitizeText(node.InnerText);
+
+            return QUOTE_TAGS.Any(tag => Regex.Split(text, tag).Length > 1);
+        }
+
+        private static string ConcatNodes(IEnumerable<HtmlNode> nodes) => string.Join(string.Empty, nodes.Select(p => p.OuterHtml));
+        private static string SanitizeText(string text) => text.Replace("&nbsp;", string.Empty).Trim();
+
+        private static readonly string[] QUOTE_TAGS = new[] {
+            "^----------",
+            @"^Em \w{3}., \d+ de \w{3}. de \d{4} às ",
+            @"^(\w*)-feira, \d+ de (\w*) de \d{4} ",
+            "^________________________________",
+            "^De:",
+            "^From:"
+        };
+    }
+}
